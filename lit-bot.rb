57cbbcc5dc4c9dd0d3827c9dc028c31363be0d82
Scott Leitch <s.d.leitch@gmail.com>
2015-11-21 12:05:16 -0500

    fucking env vars

--- lit-bot.rb
+++ lit-bot.rb
@@ -4,10 +4,10 @@ require 'chatterbot/dsl'
 chatterbot = Chatterbot::Bot #new bot
 
 #authenticate bot with Twitter API
-chatterbot.consumer_key ENV[CONSUMER_KEY]
-chatterbot.consumer_secret ENV[CONSUMER_SECRET]
-chatterbot.secret ENV[SECRET]
-chatterbot.token ENV[TOKEN]
+chatterbot.consumer_key ENV["CONSUMER_KEY"]
+chatterbot.consumer_secret ENV["CONSUMER_SECRET"]
+chatterbot.secret ENV["SECRET"]
+chatterbot.token ENV["TOKEN"]
 
 tokenizer = TactfulTokenizer::Model.new #new tokenizer
 doc = File.open('works/100YearsOfSolitude.txt') #read the .txt file
